c9742d146f183a76da2c3304b596778b2568e9fa
siugim489 <siugim489@gmail.com>
2026-06-11 09:12:49 +0900

    Create 06_수행평가.py

---
+++ pages/06_수행평가.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="나와 닮은 동물 찾기",
+    page_icon="🐾"
+)
+
+st.title("🐾 나와 닮은 동물 찾기")
+
+st.write("""
+안녕하세요! 😄
+
+이 프로그램은 간단한 질문을 통해
+나와 가장 닮은 동물을 찾아주는 프로그램입니다.
+
+왼쪽 메뉴를 눌러 시작해보세요!
+""")
